d9289b3c0e7c4b9ed29af3f378a884ed2939e5f3
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-07-17 21:03:41 +0200

    correzione snack3

--- src/snack3/RegistroStudenti.java
+++ src/snack3/RegistroStudenti.java
@@ -9,11 +9,25 @@ public class RegistroStudenti {
         studenti = new String[10];
         indiceArray = 0;
     }
+                 
+        //nadava usato insieme alla classe studente, per recuperare gli studenti da "snack1"
+        //e  non doverli riscrivere
+
+    // public void aggiungiStudente(Studente studente){
+    //     if(indiceArray < registro.length){
+    //         registro[indiceArray] = studente;
+    //         indiceArray++
+    //     }else {
+    //         System.out.println("Registro pieno.");
+    //     }
+    // }
+
+    
 
     public void AggiungiStudente(String nome, String cognome){
        if (indiceArray < studenti.length) {
-            String nomeCompleto = nome + " " + cognome;
-            studenti[indiceArray] = nomeCompleto;
+            String nomeCompleto = nome + " " + cognome;                      
+            studenti[indiceArray] = nomeCompleto;                           
             indiceArray++;
         } else {
             System.out.println("Il registro è pieno.");
@@ -26,7 +40,3 @@ public class RegistroStudenti {
         }
     }
 }
-
-
-// Snack 3: Registro Studenti
-// Crea una classe RegistroStudenti che rappresenti un registro di studenti. Utilizza un array privato per memorizzare oggetti Studente. Implementa un costruttore senza parametri per inizializzare il registro vuoto e un metodo pubblico per aggiungere studenti al registro. Aggiungi un metodo che stampi la lista degli studenti.
